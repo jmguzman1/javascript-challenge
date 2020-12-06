7960a476e9e70d613cf45d7810b3dcee45bdfee5
Julissa Guzman <julissaguzman@Julissas-MacBook-Pro.local>
2020-12-05 20:35:15 -0500

    final

--- UFO-level-1 /static/js/app.js
+++ UFO-level-1 /static/js/app.js
@@ -1,4 +1,52 @@
-// from data.js
 var tableData = data;
+console.log(tableData)
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach((ufo) => {
+
+	var row = tbody.append("tr");
+
+	Object.entries(ufo).forEach(([key, value]) => {
+
+		var cell = row.append("td");
+		cell.text(value);
+	});
+});
+
+//*** Use a date form in your HTML document and write JavaScript code that will listen
+//for events and search through the date/time column to find rows that match user input.
+
+var button = d3.select("#filter-btn");
+
+var form = d3.select("form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+
+  d3.event.preventDefault();
+
+  var inputElement = d3.select(".form-control");
+
+  var inputValue = inputElement.property("value");
+
+	var results = tableData.filter(ufo => ufo.datetime === inputValue);
+	
+	tbody.html("");
+
+	if (results.length === 0) {
+		tbody.text(`No ufo sightings on ${inputValue}.`);
+	}
+
+	else {
+		results.forEach((ufo) => {
+			var row = tbody.append("tr");
+			Object.entries(ufo).forEach(([key, value]) => {
+				var cell = row.append("td");
+				cell.text(value);
+			});
+		});
+	};
+};
